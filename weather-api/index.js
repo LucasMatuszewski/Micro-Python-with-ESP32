2041ba25b6a45270ea4e0913628aca28af2afda4
Lukasz Matuszewski <lucas.matuszewski@gmail.com>
2021-03-28 05:22:45 +0200

    Add limit endpoint, timestamp & docs links

--- weather-api/index.js
+++ weather-api/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
+// library with functions to work only with Firestore:
 const Firestore = require("@google-cloud/firestore");
+// documentation: https://googleapis.dev/nodejs/firestore/2.2.3/index.html
+
+// SDK to work with whole Firebase ecosystem:
+// const admin = require('firebase-admin');
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+// const db = admin.firestore();
+
+// Firestore queries docs: https://firebase.google.com/docs/firestore/query-data/order-limit-data
 
 const db = new Firestore();
 const app = express();
@@ -10,12 +21,27 @@ app.get("/", (req, res) => {
   res.send(`Welcome into ${name} Weather Station API!`);
 });
 
-app.get("/weather/:placeId", async (req, res) => {
-  const placeId = req.params.placeId;
-  const query = db.collection("weather-data").where("placeId", "==", placeId);
+const weatherDb = db.collection("weather-data");
+
+app.get("/weather/limit/:limit", async (req, res) => {
+  const limit = req.params.limit;
+  const query = weatherDb.limit(limit);
   const querySnapshot = await query.get();
-  if (querySnapshot.size > 0) {
-    res.json(querySnapshot.docs);
+  // if (querySnapshot.size > 0) {
+  if (querySnapshot.exists) {
+    res.status(200).json(querySnapshot.docs);
+  } else {
+    res.status(404).json({ status: `${placeID} not Found` });
+  }
+});
+
+app.get("/weather/place/:placeId", async (req, res) => {
+  const placeId = req.params.placeId;
+  const query = weatherDb.where("placeId", "==", placeId);
+  const querySnapshot = await query.get();
+  if (querySnapshot.exists) {
+    res.status(200).json(querySnapshot.docs);
+    console.log("querySnapshot.docs", querySnapshot.docs);
   } else {
     res.status(404).json({ status: `${placeID} not Found` });
   }
@@ -27,9 +53,11 @@ app.post("/weather", async (req, res) => {
     humidity: req.body.humidity,
     pressure: req.body.pressure,
     placeId: req.body.placeId,
+    created: Firestore.Timestamp.now(),
   };
-  // .doc() creates a new document, .set() sets the data into it
-  await db.collection("weather-data").doc().set(data);
+  // .doc() creates a new document with default random ID or we can specify a name for it .doc('docname')
+  // .set() sets the data into it
+  await weatherDb.doc().set(data);
   // res.status(200).json({ ...data });
   res.statusCode(200); // to send only status code without any data
 });
