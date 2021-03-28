c7c6baaa2edb23213a3e1e2c6e509c701369aadc
Lukasz Matuszewski <lucas.matuszewski@gmail.com>
2021-03-28 06:37:39 +0200

    BugFix placeId & sendStatus. Add secret API_KEY

--- weather-api/index.js
+++ weather-api/index.js
@@ -27,6 +27,7 @@ app.get("/weather/limit/:limit", async (req, res) => {
   const limit = parseInt(req.params.limit);
   const query = weatherDb.limit(limit);
   const querySnapshot = await query.get();
+  console.log("querySnapshot.docs", querySnapshot.docs);
   // if (querySnapshot.size > 0) {
   if (querySnapshot.exists) {
     res.status(200).json(querySnapshot.docs);
@@ -39,27 +40,36 @@ app.get("/weather/place/:placeId", async (req, res) => {
   const placeId = req.params.placeId;
   const query = weatherDb.where("placeId", "==", placeId);
   const querySnapshot = await query.get();
-  if (querySnapshot.exists) {
+  console.log("querySnapshot.docs", querySnapshot.docs);
+  if (querySnapshot.size > 0) {
     res.status(200).json(querySnapshot.docs);
-    console.log("querySnapshot.docs", querySnapshot.docs);
   } else {
-    res.status(404).json({ status: `${placeID} not Found` });
+    res.status(404).json({ status: `${placeId} not Found` });
   }
 });
 
 app.post("/weather", async (req, res) => {
-  const data = {
-    temperature: req.body.temperature,
-    humidity: req.body.humidity,
-    pressure: req.body.pressure,
-    placeId: req.body.placeId,
-    created: Firestore.Timestamp.now(),
-  };
-  // .doc() creates a new document with default random ID or we can specify a name for it .doc('docname')
-  // .set() sets the data into it
-  await weatherDb.doc().set(data);
-  // res.status(200).json({ ...data });
-  res.statusCode(200); // to send only status code without any data
+  // To store Secrets we can use Secret Manager: https://cloud.google.com/build/docs/securing-builds/use-secrets
+  // It works on triggered Build Time but it's not available in free tier?
+  // We can use for free this solution:
+  // https://stackoverflow.com/questions/52840187/how-to-set-environment-variables-using-google-cloud-build-or-other-method-in-goo
+  // Or we can use variables manually adding new revision: https://cloud.google.com/run/docs/configuring/environment-variables#console
+  if (req.body.secretApiKey === process.env.API_KEY) {
+    const data = {
+      temperature: req.body.temperature,
+      humidity: req.body.humidity,
+      pressure: req.body.pressure,
+      placeId: req.body.placeId,
+      created: Firestore.Timestamp.now(),
+    };
+    // .doc() creates a new document with default random ID or we can specify a name for it .doc('docname')
+    // .set() sets the data into it
+    await weatherDb.doc().set(data);
+    // res.status(200).json({ ...data });
+    res.sendStatus(200); // to send only status code with automate .send('OK')
+  } else {
+    res.sendStatus(403); // to send only status code with automate .send('Forbidden')
+  }
 });
 
 const port = process.env.PORT || 8080;
